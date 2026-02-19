f953cf448801efae9a2b0af1114c08bd07036b7f
4lph4creator <marioandre.lof@gmail.com>
2026-02-19 15:48:02 -0300

    Add caching for assets in service worker
    Implement caching strategy for service worker

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,26 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker: Installed');
+const CACHE_NAME = "lo2-cache-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./stiletto.css",
+  "./app.js",
+  "./img/bg.png",
+  "./img/logo.jpg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('activate', (e) => {
-  console.log('Service Worker: Activated');
-});
-
-self.addEventListener('fetch', (e) => {
-  // Se puede agregar cache si quieres
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
